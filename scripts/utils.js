4d0f5cd7cd37413b6dbba8db86ebeb0ebb93bf34
adisharm <adisharm@adobe.com>
2024-06-25 17:09:28 +0530

    push utils.js

---
+++ scripts/utils.js
@@ -0,0 +1,98 @@
+/*
+ * Copyright 2022 Adobe. All rights reserved.
+ * This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License. You may obtain a copy
+ * of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ * OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+
+/*
+ * ------------------------------------------------------------
+ * Edit below at your own risk
+ * ------------------------------------------------------------
+ */
+
+/**
+ * The decision engine for where to get Milo's libs from.
+ */
+export const [setLibs, getLibs] = (() => {
+  let libs;
+  return [
+    (prodLibs) => {
+      const { hostname } = window.location;
+      if (!hostname.includes('hlx.page')
+        && !hostname.includes('hlx.live')
+        && !hostname.includes('localhost')) {
+        libs = prodLibs;
+        return libs;
+      }
+      const branch = new URLSearchParams(window.location.search).get('milolibs') || 'main';
+      if (branch === 'local') return 'http://localhost:6456/libs';
+      if (branch.indexOf('--') > -1) return `https://${branch}.hlx.page/libs`;
+      return `https://${branch}--milo--adobecom.hlx.page/libs`;
+    }, () => libs,
+  ];
+})();
+
+/**
+ * Gets placeholders object
+ * @param {string} prefix
+ */
+export async function fetchPlaceholders(prefix = 'default') {
+  window.placeholders = window.placeholders || {};
+  const loaded = window.placeholders[`${prefix}-loaded`];
+  if (!loaded) {
+    window.placeholders[`${prefix}-loaded`] = new Promise((resolve, reject) => {
+      const url = `${prefix === 'default' ? '' : prefix}/placeholders.json`;
+      fetch(url)
+        .then((resp) => {
+          if (!resp.ok) {
+            reject(new Error(`fetch ${url}: ${resp.status} - ${resp.statusText}`));
+          }
+          return resp.json();
+        })
+        .then((json) => {
+          const placeholders = {};
+          json.data.forEach((placeholder) => {
+            placeholders[toCamelCase(placeholder.Key)] = placeholder.Text;
+          });
+          window.placeholders[prefix] = placeholders;
+          resolve();
+        })
+        .catch((e) => {
+          reject(e);
+        });
+    });
+  }
+  try {
+    await window.placeholders[`${prefix}-loaded`];
+    return (window.placeholders[prefix]);
+  } catch(e) {
+    console.error(`get ${prefix} placeholders: ${e}`);
+    return {};
+  }
+}
+
+/**
+ * Sanitizes a name for use as class name.
+ * @param {string} name The unsanitized name
+ * @returns {string} The class name
+ */
+export function toClassName(name) {
+  return name && typeof name === 'string'
+    ? name.toLowerCase().replace(/[^0-9a-z]/gi, '-')
+    : '';
+}
+
+/*
+ * Sanitizes a name for use as a js property name.
+ * @param {string} name The unsanitized name
+ * @returns {string} The camelCased name
+ */
+export function toCamelCase(name) {
+  return toClassName(name).replace(/-([a-z])/g, (g) => g[1].toUpperCase());
+}
